8e9e6eb4ab482566923f97edeaa661cb948ccf8e
Jacob Burke <jwburke256@gmail.com>
2022-11-16 16:58:17 -0800

    lab3 everything minus delete commit

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -83,16 +83,25 @@ class Tree(object):
         # returns the node with that particular data value else returns None
         if self.root is None or data is None:
             return None
-        if data < self.root:
-            return self.__find_node(self.left, data)
-        else:
-            return self.__find_node(self.right, data)
+        node = self.root
+        while node is not None:
+            if data == node.data:
+                return node
+            if data < node.data:
+                node = node.left
+            else:
+                node = node.right
+        return None
 
     def contains(self, data):
         # return True of node containing data is present in the tree.
         # otherwise, return False.
         # you may use a helper method __find_node() to find a particular node with the data value and return that node
-        pass
+        found = self.__find_node(data)
+        if found is None:
+            return False
+        else:
+            return True
 
     def __iter__(self):
         # iterate over the nodes with inorder traversal using a for loop
@@ -100,15 +109,15 @@ class Tree(object):
 
     def inorder(self):
         # inorder traversal : (LEFT, ROOT, RIGHT)
-        return self.__traverse(self.root, Tree.INORDER)
+        return self.__traverse(self.root, 1)
 
     def preorder(self):
         # preorder traversal : (ROOT, LEFT, RIGHT)
-        return self.__traverse(self.root, Tree.PREORDER)
+        return self.__traverse(self.root, 2)
 
     def postorder(self):
         # postorder traversal : (LEFT, RIGHT, ROOT)
-        return self.__traverse(self.root, Tree.POSTORDER)
+        return self.__traverse(self.root, 3)
 
     def __traverse(self, curr_node, traversal_type):
         # helper method implemented using generators
@@ -116,13 +125,18 @@ class Tree(object):
         
         #Yield data of the correct node/s
         if curr_node is not None:
-            if traversal_type is Tree.inorder():
+            if traversal_type is 1:
                 yield from self.__traverse(curr_node.left, traversal_type)
-
-            elif traversal_type == 2:
-
-            elif traversal_type == 3:
-
+                yield curr_node.data
+                yield from self.__traverse(curr_node.right, traversal_type)
+            if traversal_type is 2:
+                yield curr_node.data
+                yield from self.__traverse(curr_node.left, traversal_type)
+                yield from self.__traverse(curr_node.right, traversal_type)
+            if traversal_type is 3:
+                yield from self.__traverse(curr_node.left, traversal_type)
+                yield from self.__traverse(curr_node.right, traversal_type)
+                yield curr_node.data
         else:
             return None
 
@@ -138,7 +152,45 @@ class Tree(object):
         # Note: Make sure to handle the case where the parent is None
     
     	# Return object of successor if found else return None
-        pass
+        node = self.__find_node(data)
+        try:
+            if node is None:
+                raise KeyError
+        except KeyError:
+            print("Given node does not exist in current Binary Search Tree")
+            raise KeyError
+        # Right subtree not empty, successor is leftmost node in right subtree or right node itself
+        if node.right is not None:
+            node = node.right
+            while node is not None:
+                prevNode = node
+                node = node.left
+            return prevNode
+        # Right subtree empty, find lowest ancestor of parent that will be successor
+        elif node.right is None:
+            try:
+                if node.parent is None:
+                    raise KeyError
+            except KeyError:
+                print("Successor does not exist in current Binary Search Tree")
+                raise KeyError
+            y = node.parent
+            while ((y is not None) and (node == y.right)):
+                node = y
+                y = y.parent
+            return y
+        else:
+            return None
+
+    def __transplant(self, parent, child):
+        if parent.parent is None:
+            self.root = child
+        elif parent == parent.parent.left:
+            parent.parent.left = child
+        else:
+            parent.parent.right = child
+        if child is None:
+            child.parent = parent.parent
 
     def delete(self, data):
         # Find the node to delete.
@@ -150,7 +202,49 @@ class Tree(object):
         #       successor from its previous location.
         # Recall: The successor of a node is the left-most node in the node's right subtree.
         # Note: Make sure to handle the case where the parent is None
-    
-        pass
+
+        node = self.__find_node(data)
+        try:
+            if node is None:
+                raise KeyError
+        except KeyError:
+            print("Given node does not exist in current Binary Search Tree")
+            raise KeyError
+        if node.left is None:
+            self.__transplant(node, node.right)
+        elif node.right is None:
+            self.__transplant(node, node.left)
+        else:
+            y = self.min()
+            if y is not node.right:
+                self.__transplant(y, y.right)
+                y.right = node.right
+                y.right.parent = y
+            self.__transplant(node, y)
+            y.left = node.left
+            y.left.parent = node
+        return None
+
+
+
+        # node = self.__find_node(data)
+        # try:
+        #     if node is None:
+        #         raise KeyError
+        # except KeyError:
+        #     print("Given node does not exist in current Binary Search Tree")
+        #     raise KeyError
+        # # The node is root
+        # if (node.left is None) and (node.right is None) and (node.parent is None):
+        #     self.root = None
+        # # The node has no children and is
+        # elif (node.left is None) and (node.right is None):
+        #     node.parent = None
+        # # The node has one child
+        # elif ((node.left is not None) and (node.right is None)) or ((node.left is None) and (node.right is not None)):
+        #     if node.left is not None:
+        #         child = node.left
+        #         parent = node.parent
+        #         parent.
 
 
