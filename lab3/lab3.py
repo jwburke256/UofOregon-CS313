fc63f800f2693f8bfb03b2eaf6fb356ec61d95ad
jwburke256 <jwburke256@gmail.com>
2022-11-16 22:16:28 -0800

    Finalized lab3 and submitted

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -1,4 +1,22 @@
 class Node(object):
+    """
+    A class to represent a node in a binary search tree.
+
+    ...
+
+    Attributes
+    ----------
+    parent : object of class Node
+        A pointer to the parent node of the current node in a binary search tree
+    left : object of class Node
+        A pointer to the left child node of the current node in a binary search
+        tree
+    right : object of class Node
+        A pointer to the right child node of the current node in a binary
+        search tree
+    data : int
+        An individual number to be stored in a node
+    """
     def __init__(self, data):
         self.parent = None
         self.left = None
@@ -7,6 +25,76 @@ class Node(object):
 
 
 class Tree(object):
+    """Binary Search Tree
+
+        Supports most standard Binary Search Tree operations (insert, min,
+        max, contains, find_successor, and contains).
+        Can be used for storing integer data in a sorted order, and then able to
+        be printed out in a specified manner with calling the desired traversal
+        method (inorder, preorder, postorder). Tree has a maximum height of
+        log n.
+        ...
+
+
+        Attributes
+        ----------
+        root: int
+            first node in the Binary Search Tree
+
+        Methods
+        -------
+        print():
+            Prints out the data of all nodes in order
+        __print(curr_node):
+            Recursively prints a subtree (in order), rooted at curr_node
+        insert(data):
+            inserts data into the binary search tree by traversing the tree
+            to one of the leaves and inserts as one of the previous null
+            children to become the new leaf
+        min():
+            Returns the minimum value held in the tree.
+            Returns None if the tree is empty
+        max():
+            Returns the maximum value held in the tree.
+            Returns None if the tree is empty
+        __find_node(data):
+            Returns the node with that particular data value. If data not found
+            it returns None instead.
+        contains(data):
+            Returns True if node containing data is present in the tree.
+            Otherwise, function returns False.
+        ____iter__():
+            iterate over the nodes with inorder traversal using a for loop
+        inorder():
+            Used in conjunction with __traverse, returns the binary search
+            tree values inorder
+        preorder():
+            Used in conjunction with __traverse, returns the binary search
+            tree values preorder
+        postorder():
+            Used in conjunction with __traverse, returns the binary search
+            tree values postorder
+        __traverse(curr_node, traversal_type):
+            helper method for traversal functions, implemented using generators
+            to return the binary search tree values based on given
+            traversal_type
+        find_successor(data):
+            Find the successor node in a given Binary Search Tree based off the
+            data argument. If the value specified by find_successor does NOT
+            exist in the tree, then the function raises a KeyError exception. If
+            no successor exists for a given value that exists in the tree, then
+            the function returns None
+        __transplant(parent, child):
+            Helper function for delete that moves a subtree of a deleted node
+            into the correct position.
+        delete(data):
+            Find the node to delete. If the value specified by delete does NOT
+            exist in the tree, then the function doesn't change the tree and
+            instead raises a KeyError exception. If node found function utilizes
+            transplant as needed to delete the node and adjust the tree.
+        """
+
+
     # Binary Search Tree
     # class constants
     PREORDER = 1
@@ -80,7 +168,8 @@ class Tree(object):
             return x.data
 
     def __find_node(self, data):
-        # returns the node with that particular data value else returns None
+        """Returns the node with that particular data value. If data not found
+        it returns None instead."""
         if self.root is None or data is None:
             return None
         node = self.root
@@ -94,9 +183,10 @@ class Tree(object):
         return None
 
     def contains(self, data):
-        # return True of node containing data is present in the tree.
-        # otherwise, return False.
-        # you may use a helper method __find_node() to find a particular node with the data value and return that node
+        """Returns True if node containing data is present in the tree.
+        Otherwise, function returns False."""
+        # you may use a helper method __find_node() to find a particular node
+        # with the data value and return that node
         found = self.__find_node(data)
         if found is None:
             return False
@@ -104,25 +194,32 @@ class Tree(object):
             return True
 
     def __iter__(self):
-        # iterate over the nodes with inorder traversal using a for loop
+        """iterate over the nodes with inorder traversal using a for loop"""
         return self.inorder()
 
     def inorder(self):
+        """Used in conjunction with __traverse, returns the binary search
+        tree values inorder"""
         # inorder traversal : (LEFT, ROOT, RIGHT)
         return self.__traverse(self.root, 1)
 
     def preorder(self):
+        """Used in conjunction with __traverse, returns the binary search
+        tree values preorder"""
         # preorder traversal : (ROOT, LEFT, RIGHT)
         return self.__traverse(self.root, 2)
 
     def postorder(self):
+        """Used in conjunction with __traverse, returns the binary search
+        tree values postorder"""
         # postorder traversal : (LEFT, RIGHT, ROOT)
         return self.__traverse(self.root, 3)
 
     def __traverse(self, curr_node, traversal_type):
-        # helper method implemented using generators
+        """helper method for traversal functions, implemented using generators
+        to return the binary search tree values based on given traversal_type"""
         # all the traversals can be implemented using a single method
-        
+
         #Yield data of the correct node/s
         if curr_node is not None:
             if traversal_type == 1:
@@ -141,14 +238,17 @@ class Tree(object):
             return None
 
     def find_successor(self, data):
-        # Find the successor node
-        # If the value specified by find_successor does NOT exist in the tree, then raise a KeyError
-        # helper method to implement the delete method but may be called on its own
-        # If the right subtree of the node is nonempty,then the successor is just 
-        # the leftmost node in the right subtree.
-        # If the right subtree of the node is empty, then go up the tree until a node that is
-        # the left child of its parent is encountered. The parent of the found node will be the
-        # successor to the initial node.
+        """Find the successor node in a given Binary Search Tree based off the
+        data argument. If the value specified by find_successor does NOT
+        exist in the tree, then the function raises a KeyError exception. If
+        no successor exists for a given value that exists in the tree, then
+        the function returns None"""
+        # helper method to implement the delete method but may be called on its
+        # own. If the right subtree of the node is nonempty,then the
+        # successor is just the leftmost node in the right subtree.
+        # If the right subtree of the node is empty, then go up the tree until
+        # a node that is the left child of its parent is encountered. The
+        # parent of the found node will be the successor to the initial node.
         # Note: Make sure to handle the case where the parent is None
     
     	# Return object of successor if found else return None
@@ -186,6 +286,8 @@ class Tree(object):
             return None
 
     def __transplant(self, parent, child):
+        """Helper function for delete that moves a subtree of a deleted node
+        into the correct position."""
         if parent.parent is None:
             self.root = child
         elif parent == parent.parent.left:
@@ -196,14 +298,17 @@ class Tree(object):
             child.parent = parent.parent
 
     def delete(self, data):
-        # Find the node to delete.
-        # If the value specified by delete does NOT exist in the tree, then don't change the tree and raise a KeyError
+        """Find the node to delete. If the value specified by delete does NOT
+        exist in the tree, then the function doesn't change the tree and
+        instead raises a KeyError exception. If node found function utilizes
+        transplant as needed to delete the node and adjust the tree."""
         # If you find the node and ...
         #  a) The node has no children, just set it's parent's pointer to None.
         #  b) The node has one child, make the nodes parent point to its child.
-        #  c) The node has two children, replace it with its successor, and remove
-        #       successor from its previous location.
-        # Recall: The successor of a node is the left-most node in the node's right subtree.
+        #  c) The node has two children, replace it with its successor, and
+        #  remove successor from its previous location.
+        # Recall: The successor of a node is the left-most node in the node's
+        # right subtree.
         # Note: Make sure to handle the case where the parent is None
 
         node = self.__find_node(data)
--- lab3/test_lab3.py
+++ lab3/test_lab3.py
@@ -32,7 +32,6 @@ class T0_tree__insert(unittest.TestCase):
         print("\n")
 
     def test_unbalanced_binary_search_tree(self):
-        # Added test
         print("\n")
         print("tree_insert_with_individual_check")
         t = lab3.Tree()
@@ -45,11 +44,6 @@ class T0_tree__insert(unittest.TestCase):
         t.insert(6)
         t.insert(7)
         maximum = t.max()
-
-        # The following check is without using tree as an iterator (which
-        # uses inorder traversal)
-        # So this function also does not check the implementation of the
-        # traversal function
 
         self.assertEqual(t.root.data, 1)
         self.assertEqual(t.root.left, None)
@@ -84,7 +78,6 @@ class T1_min_and_max(unittest.TestCase):
         print("\n")
 
     def test_min_and_max_unbalanced_tree(self):
-        # added test
         print("\n")
         print("tree_insert_with_individual_check")
         t = lab3.Tree()
@@ -129,8 +122,25 @@ class T2_Traversal(unittest.TestCase):
         print("\n")
         print("Checking all the three traverses")
         t = lab3.Tree()
-        t._Tree__traverse
+        t.insert(4)
+        t.insert(2)
+        t.insert(6)
+        t.insert(1)
+        t.insert(3)
+        t.insert(5)
+        t.insert(7)
 
+        tree_inorder = [node for node in t._Tree__traverse(t.root, 1)]
+        tree_preorder = [node for node in t._Tree__traverse(t.root, 2)]
+        tree_postorder = [node for node in t._Tree__traverse(t.root, 3)]
+
+        print("inorder traversal")
+        self.assertEqual(tree_inorder, [1, 2, 3, 4, 5, 6, 7])
+        print("preorder traversal")
+        self.assertEqual(tree_preorder, [4, 2, 1, 3, 6, 5, 7])
+        print("postorder traversal")
+        self.assertEqual(tree_postorder, [1, 3, 2, 5, 7, 6, 4])
+        print("\n")
 
 
 class T3_successor(unittest.TestCase):
@@ -168,13 +178,10 @@ class T3_successor(unittest.TestCase):
         print("\n")
 
     def test_successor_single_node(self):
-        # added test case
         print("\n")
         print("successor function with single node")
         tree_success = lab3.Tree()
         tree_success.insert(8)
-        # max = tree_success.max()
-        # print(max)
         none_success = tree_success.find_successor(8)
 
         self.assertEqual(none_success, None)
@@ -206,8 +213,6 @@ class T4_delete(unittest.TestCase):
         t.delete(10)
         l5 = [node for node in t]
 
-
-
         self.assertEqual(l1, [1, 3, 4, 6, 7, 8, 10, 13, 14])
         self.assertEqual(l2, [1, 3, 4, 6, 8, 10, 13, 14])
         self.assertEqual(l3, [1, 3, 4, 8, 10, 13, 14])
@@ -225,7 +230,6 @@ class T4_delete(unittest.TestCase):
         print("\n")
 
     def test_delete_node_not_in_tree(self):
-        # added test case
         print("\n")
         print("delete function with a given node not in the tree")
         t = lab3.Tree()
@@ -264,17 +268,35 @@ class T5_contains(unittest.TestCase):
         print("\n")
 
     def test_contains_empty_tree(self):
-        # added test case
         print("\n")
         print("contains function")
         t = lab3.Tree()
         self.assertEqual(t.contains(15), False)
+        print("\n")
 
     def test__find_node(self):
-        # added test case
         print("\n")
         print("find node private function")
         t = lab3.Tree()
+        t.insert(8)
+        t.insert(3)
+        t.insert(10)
+        t.insert(1)
+        t.insert(6)
+        t.insert(4)
+        t.insert(7)
+        t.insert(14)
+        t.insert(13)
+
+        node1 = t._Tree__find_node(1)
+        node2 = t._Tree__find_node(6)
+        node3 = t._Tree__find_node(5)
+
+        self.assertEqual(node1.data, 1)
+        self.assertEqual(node2.data, 6)
+        self.assertEqual(node3, None)
+        print("\n")
+
 
 
 if __name__ == '__main__' :
